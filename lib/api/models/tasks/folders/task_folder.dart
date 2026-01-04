3c68a66e6ddc088d9930a5fdd12093ac78cb0cd8
danila <danilapayne@gmail.com>
2026-01-04 15:03:54 +0500

    Added riverpod state and added folder's provider

--- lib/api/models/tasks/folders/task_folder.dart
+++ lib/api/models/tasks/folders/task_folder.dart
@@ -24,4 +24,20 @@ class TaskFolder {
     createdAt: json['created_at'] as String,
     updatedAt: json['updated_at'] as String,
   );
+
+  TaskFolder copyWith({
+    int? id,
+    String? name,
+    List<TaskCategory>? categories,
+    String? createdAt,
+    String? updatedAt,
+  }) {
+    return TaskFolder(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      categories: categories ?? this.categories,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
 }
---
+++ lib/providers/tasks/task_folders_provider.dart
@@ -0,0 +1,43 @@
+﻿import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:marquer/api/models/tasks/categories/task_category.dart';
+import 'package:marquer/api/models/tasks/folders/task_folder.dart';
+import 'package:marquer/api/services/tasks_service.dart';
+
+final taskFoldersProvider = AsyncNotifierProvider<TaskFoldersAsyncNotifier, List<TaskFolder>>(
+  TaskFoldersAsyncNotifier.new,
+);
+
+class TaskFoldersAsyncNotifier extends AsyncNotifier<List<TaskFolder>> {
+  final _service = TasksService();
+
+  @override
+  Future<List<TaskFolder>> build() async {
+    return _service.getFolders();
+  }
+
+  void deleteCategory(int folderId, int categoryId) {
+    final current = state.asData?.value;
+    if (current == null) return;
+
+    state = AsyncData([
+      for (final f in current)
+        if (f.id == folderId)
+          f.copyWith(
+            categories: [
+              for (final c in f.categories)
+                if (c.id != categoryId) c,
+            ],
+          )
+    ]);
+  }
+
+  void updateCategories(int folderId, List<TaskCategory> cats) {
+    final current = state.asData?.value;
+    if (current == null) return;
+
+    state = AsyncData([
+      for (final f in current)
+        if (f.id == folderId) f.copyWith(categories: cats) else f,
+    ]);
+  }
+}
